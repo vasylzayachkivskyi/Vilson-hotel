5523a78c901a0ed59bda34afa01c94b9b76450c8
vasylzayachkivskyi <vasylzaya@gmail.com>
2023-11-14 16:41:20 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
 
 $(document).ready(function () {
 
+      // change header while scrolling --------------------------- //
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 10) {
+            $('.header').addClass('change-bg');
+        } else {
+            $('.header').removeClass('change-bg');
+        }
+    });
+
     // submenu open ----------------- //
     $('.sub-menu').on('click', function () {
         $(this).toggleClass('active');
         $(this).children('ul').slideToggle();
     });
 
-     // mobile menu ----------------- //
-     $('.burger-btn').on('click', function () {
+    // mobile menu ----------------- //
+    $('.burger-btn').on('click', function () {
         $(this).toggleClass('active');
         $('.header__nav').toggleClass('active');
     });
-
-
-
-
-
 
     // heroscreen slider ------------ //
     var swiper = new Swiper(".heroscreen__slider", {
@@ -30,6 +34,36 @@ $(document).ready(function () {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         }
+    })
+
+    // padding(imitation container) -------------------------- //
+    var windowWidth = $(document).width(),
+        containerWidth = $('.container').width(),
+        paddingContainer = ((windowWidth - containerWidth) / 2);
+    $('.margleft').css('margin-left', paddingContainer);
+    $('.margright').css('margin-right', paddingContainer);
+    $('.padright').css('padding-right', paddingContainer);
+    $('.padleft').css('padding-left', paddingContainer);
+
+    $(window).resize(function () {
+        var windowWidth = $(document).width(),
+            containerWidth = $('.container').width(),
+            paddingContainer = ((windowWidth - containerWidth) / 2);
+        $('.margleft').css('margin-left', paddingContainer);
+        $('.margright').css('margin-right', paddingContainer);
+        $('.padright').css('padding-right', paddingContainer);
+        $('.padleft').css('padding-left', paddingContainer);
+    });
+
+    // events slider ------------ //
+    var swiper = new Swiper(".events__slider", {
+        loop: true,
+        slidesPerView: 'auto',
+        spaceBetween: 35,
+        speed: 7000,
+        autoplay: {
+            delay: 0,
+        },
     })
 
 
