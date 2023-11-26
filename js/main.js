86f83a2ff67ff51ea8d874efab95aa5dc1f6fa59
vasylzayachkivskyi <vasylzaya@gmail.com>
2023-11-26 20:27:09 +0200

    add page terms

--- js/main.js
+++ js/main.js
@@ -297,6 +297,64 @@ $(document).ready(function () {
     });
 
 
+    //  terms navigation
+    if ($('.terms__nav').length > 0) {
+        var headers = document.querySelectorAll('.terms__content h2');
+        for (var i = 0; i < headers.length; i++) {
+            headers[i].setAttribute('id', i + 1);
+        }
+
+
+        var headers = document.querySelectorAll('.terms__content h2');
+        var list = document.querySelector('.terms__nav');
+        var ul = document.createElement('ul');
+
+        for (var i = 0; i < headers.length; i++) {
+            var text = headers[i].textContent;
+            var id = headers[i].getAttribute('id');
+            var listItem = document.createElement('li');
+            var link = document.createElement('a');
+
+            link.textContent = text;
+            link.setAttribute('href', '#' + id);
+            listItem.appendChild(link);
+            ul.appendChild(listItem);
+        }
+
+        list.appendChild(ul);
+    }
+
+    // -----------
+
+
+        $(".terms__nav ul").on("click", "a", function (event) {
+            event.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({ scrollTop: top - 105 }, 0);
+        });
+
+
+        const links = document.querySelectorAll('.terms__nav li a');
+        window.addEventListener('scroll', () => {
+            const anchors = document.querySelectorAll('.terms__content [id]');
+            anchors.forEach(anchor => {
+                if (anchor.closest('.terms__content')) {
+                    const anchorTop = anchor.getBoundingClientRect().top + window.pageYOffset;
+                    if (window.pageYOffset >= anchorTop - 110) {
+                        links.forEach(link => {
+                            const correspondingLink = document.querySelector(`.terms__nav li a[href="#${anchor.id}"]`);
+                            if (correspondingLink) {
+                                link.classList.remove('active');
+                                correspondingLink.classList.add('active');
+                            }
+                        });
+                    }
+                }
+            });
+        });
+
+
 });
 
 
